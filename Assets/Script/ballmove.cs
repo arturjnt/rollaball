ce4a280f96ffefab0a49d0605077c5c1d2b3a00e
Artur Teixeira <arturjnt@gmail.com>
2015-05-27 08:46:40 +0100

    Initial commit.

---
+++ Assets/Script/ballmove.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ballmove : MonoBehaviour 
+{
+	public float ballSpeed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count = 12;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		setCountText ();
+		winText.text = "";
+	}
+
+	void setCountText()
+	{
+		countText.text = "Faltam> " + count.ToString();
+		if (count <= 0) {
+			winText.text = "Conseguiste em: " + Time.time.ToString();
+		}
+	}
+
+	void FixedUpdate() 
+	{
+		float moveHor = Input.GetAxis ("Horizontal");
+		float moveVer = Input.GetAxis ("Vertical"); 	
+
+		Vector3 v3 = new Vector3 (moveHor, 0.0f, moveVer);
+		rb.AddForce (v3 * ballSpeed);
+
+		//rb.transform.Translate (Input.acceleration.x, 0, -Input.acceleration.z);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count--;
+			setCountText();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+### Roll-A-Ball
+
+* Unity test game
+
+### Goal
+
+* Catch all the gems in the shortest amount of time.
